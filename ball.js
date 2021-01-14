0eb2b79947f92e5d56522261c4059f3d7bce51c8
VivaanBattish <73653348+VivaanBattish@users.noreply.github.com>
2021-01-14 21:04:14 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,25 @@
+class Ball{
+    constructor(x, y) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.circle(x, y, 25, options);
+        
+        this.width = 25;
+        this.height = 25;
+        
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        ellipseMode(RADIUS);
+        fill(0,0,0);
+        ellipse(0, 0, this.width, this.height);
+        pop();
+      }
+}
