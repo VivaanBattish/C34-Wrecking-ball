56b7dda36ae237c57d2e24eb4eb03866832430e1
VivaanBattish <73653348+VivaanBattish@users.noreply.github.com>
2021-01-21 17:08:29 +0530

    Update machine.js

--- machine.js
+++ machine.js
@@ -9,9 +9,11 @@ class machine {
       World.add(world, this.body);
     }
     display(){
+        push();
       var pos =this.body.position;
       rectMode(CENTER);
       fill("yellow");
       rect(pos.x, pos.y, this.width, this.height);
+        pop();
     }
   };
